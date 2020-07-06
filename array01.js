849bbd91ee5abe88ab4c8b28ebbec62435fb90d2
Tunglone <62212213+Tunglone@users.noreply.github.com>
2020-07-06 16:18:58 +0700

    Create array01.js

---
+++ array01.js
@@ -0,0 +1,17 @@
+   var studentList = [
+       {
+           name: 'Nguyễn Văn A',
+           sex: 'male',
+           age: 21
+        },
+        {
+            name: 'Nguyễn Văn B',
+            sex: 'male',
+            age: 21
+         },
+         {
+            name: 'Nguyễn Thị C',
+            sex: 'female',
+            age: 21
+         }
+   ];
